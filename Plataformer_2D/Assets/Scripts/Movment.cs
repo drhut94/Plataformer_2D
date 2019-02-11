3833205485c29ce3d65fb35d91077029741ada1f
Jan Jongejeugd Serrat <drhut94@gmail.com>
2019-02-11 23:10:27 +0100

    Versión_1.2.0
    Implementadas algunas mecánicas de movimiento

---
+++ Plataformer_2D/Assets/Scripts/Movment.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movment : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    protected float moveHorizontal;
+    protected Vector2 v2;
+    public float force;
+    public float maxVelocity;
+
+
+	void Start () {
+
+	}
+	
+
+	void Update () {
+
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
+
+    }
+
+    private void FixedUpdate()
+    {
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        v2.x = force * moveHorizontal * Time.deltaTime;
+
+        if (rb.velocity.x > maxVelocity)
+        {
+            rb.velocity = new Vector2 (maxVelocity, 0);
+        }
+
+        rb.AddForce(v2);
+    }
+}
