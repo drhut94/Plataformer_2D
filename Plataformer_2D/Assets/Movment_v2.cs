be9b2fe71704817e2132f31d0df77110fc18094f
Jan Jongejeugd Serrat <drhut94@gmail.com>
2019-02-12 22:58:47 +0100

    lolaso
    vale la he liado que flipas y basicamente el personaje se mueve pero como el culo

---
+++ Plataformer_2D/Assets/Movment_v2.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movment_v2 : MonoBehaviour {
+
+
+    Rigidbody2D rb;
+    public float speed;
+    Vector2 speedV2;
+    public float force = 10;
+    protected float moveHorizontal;
+    enum Status {
+        idle,
+        accelerate,
+        move,
+        jump,
+        crouch
+    }
+
+    Status mode = Status.idle;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+
+void Update () {
+
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        Debug.Log(mode);
+        
+
+        if (moveHorizontal != 0)
+        {
+            mode = Status.accelerate;
+
+            if (rb.velocity.x >= speed * moveHorizontal
+                || rb.velocity.x <= -speed * moveHorizontal)
+            {
+                mode = Status.move;
+            }
+        }
+        else
+        {
+            mode = Status.idle;
+        }
+
+        switch (mode)
+        {
+            case Status.idle:
+                
+                if (rb.velocity.x != 0)
+                {
+                    rb.AddForce(transform.right * force / 10 * -1);
+                }
+                break;
+
+            case Status.accelerate:
+                rb.AddForce(transform.right * force * moveHorizontal);
+                mode = Status.idle;
+                break;
+
+            case Status.move:
+                speedV2.x =  speed * moveHorizontal;
+                rb.velocity = speedV2;
+                mode = Status.idle;
+                break;
+
+            case Status.jump:
+
+                break;
+
+            case Status.crouch:
+
+                break;
+        }
+    }
+
+
+}
+
+
